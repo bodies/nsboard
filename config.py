483cd216ced7e04f88da31e717cd03b3bf0881f7
nodance.net <yoonjo@gmail.com>
2015-11-16 18:57:31 +0900

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+SITE_NAME = '꿀단지'
+
+DB_HOST = 'localhost'
+DB_SOCKET = '/home/bodies/var/www/uwsgi_ns.socket'
+DB_NAME = 'nsboard'
+DB_USER = 'bodies'
+# DB_PASSWORD = '123456'
+DB_PASSWORD = 'june9425'
+DB_PORT = 3306
